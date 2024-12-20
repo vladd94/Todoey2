946b6447feb6b27ced4a5a118e2b13a366a31d26
vladd94 <finch-06.bluish@icloud.com>
2024-12-20 14:39:51 -0600

    Initial Commit

---
+++ Todoey2/Todoey2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todoey2App.swift
+//  Todoey2
+//
+//  Created by Vlad Stoicoviciu on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Todoey2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todoey2Tests/Todoey2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Todoey2Tests.swift
+//  Todoey2Tests
+//
+//  Created by Vlad Stoicoviciu on 12/20/24.
+//
+
+import Testing
+@testable import Todoey2
+
+struct Todoey2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
